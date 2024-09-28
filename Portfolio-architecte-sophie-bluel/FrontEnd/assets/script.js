dc4377af953ddecabbdca83b8a9be11fec0cfe45
Elisabeth Decambourg <de-cambourg.elisabeth@orange.fr>
2024-09-28 16:59:47 +0200

    1.2, créa boutons filtres + css

--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
@@ -1,5 +1,5 @@
 
-
+/* affichage homepage*/
 async function homePage() {
 const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
 const sectionProjet = document.querySelector(".gallery");
@@ -18,3 +18,22 @@ for (let i = 0; i < projet.length; i++) {
 }}
 
 homePage();
+
+/* création des boutons filtres*/
+
+
+const btnfilterT = document.createElement("button");
+btnfilterT.innerText = `Tous`;
+const btnfilterO = document.createElement("button");
+btnfilterO.innerText = `Objets`;
+const btnfilterA = document.createElement("button");
+btnfilterA.innerText = `Appartements`;
+const btnfilterH = document.createElement("button");
+btnfilterH.innerText = `Hotels & restaurants`
+
+const placeMenu = document.querySelector(".placemenu");
+placeMenu.appendChild(btnfilterT);
+placeMenu.appendChild(btnfilterO);
+placeMenu.appendChild(btnfilterA);
+placeMenu.appendChild(btnfilterH);
+
