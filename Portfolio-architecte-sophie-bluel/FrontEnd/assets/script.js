8985a3452258a45eceb8384e9387afe30145e028
Elisabeth Decambourg <de-cambourg.elisabeth@orange.fr>
2024-09-28 19:15:06 +0200

    ajout add-eventListener

--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
@@ -1,26 +1,4 @@
-
-/* affichage homepage*/
-async function homePage() {
-const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
-const sectionProjet = document.querySelector(".gallery");
-
-for (let i = 0; i < projet.length; i++) {
-    const creaElement = document.createElement("figure");
-    const imageElement = document.createElement("img");
-    imageElement.src = projet[i].imageUrl;
-    const titreElement = document.createElement("figcaption");
-    titreElement.innerText = projet[i].title;
-
-    creaElement.appendChild(imageElement);
-    creaElement.appendChild(titreElement);
-
-    sectionProjet.appendChild(creaElement);
-}}
-
-homePage();
-
 /* création des boutons filtres*/
-
 
 const btnfilterT = document.createElement("button");
 btnfilterT.innerText = `Tous`;
@@ -36,4 +14,76 @@ placeMenu.appendChild(btnfilterT);
 placeMenu.appendChild(btnfilterO);
 placeMenu.appendChild(btnfilterA);
 placeMenu.appendChild(btnfilterH);
+
+
+
+/* affichage homepage*/
+async function homePage(){
+    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
+    generer(projet);
+}
+
+function generer(affiche) {
+    for (let i = 0; i < affiche.length; i++) {
+        const creaElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = affiche[i].imageUrl;
+        const titreElement = document.createElement("figcaption");
+        titreElement.innerText = affiche[i].title;
+
+        creaElement.appendChild(imageElement);
+        creaElement.appendChild(titreElement);
+
+        const sectionProjet = document.querySelector(".gallery");
+        sectionProjet.appendChild(creaElement);
+    }
+}
+
+homePage()
+
+
+
+btnfilterO.addEventListener("click", async () =>{
+    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
+    const objet = projet.filter(function(x){
+        return x.category.id ===1;
+    })
+
+    document.querySelector(".gallery").innerHTML="";
+
+    generer(objet)
+
+})
+
+btnfilterA.addEventListener("click", async () =>{
+    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
+    const objet = projet.filter(function(x){
+        return x.category.id ===2;
+    })
+
+    document.querySelector(".gallery").innerHTML="";
+
+    generer(objet)
+
+})
+btnfilterH.addEventListener("click", async () =>{
+    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
+    const objet = projet.filter(function(x){
+        return x.category.id ===3;
+    })
+
+    document.querySelector(".gallery").innerHTML="";
+
+    generer(objet)
+
+})
+
+btnfilterT.addEventListener("click", async () =>{
+    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
+   
+    document.querySelector(".gallery").innerHTML="";
+
+    generer(projet)
+
+})
 
