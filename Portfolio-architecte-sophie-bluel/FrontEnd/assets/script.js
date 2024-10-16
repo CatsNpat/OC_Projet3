9b831d0bc2687d352e8e450a857e71c7dee98678
Elisabeth Decambourg <de-cambourg.elisabeth@orange.fr>
2024-10-16 13:27:21 +0200

    suppression dans la modal + mise au propre et factorisation du code 2

--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/script.js
@@ -17,29 +17,6 @@ placeMenu.appendChild(btnfilterH);
 
 
 
-/* affichage homepage*/
-async function homePage(){
-    const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
-    generer(projet);
-}
-
-
-function generer(affiche) {
-    for (let i = 0; i < affiche.length; i++) {
-        const creaElement = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        imageElement.src = affiche[i].imageUrl;
-        const titreElement = document.createElement("figcaption");
-        titreElement.innerText = affiche[i].title;
-
-        creaElement.appendChild(imageElement);
-        creaElement.appendChild(titreElement);
-
-        const sectionProjet = document.querySelector(".gallery");
-        sectionProjet.appendChild(creaElement);
-    }
-}
-
 homePage()
 
 /*partie boutons filtre*/
@@ -67,6 +44,7 @@ btnfilterA.addEventListener("click", async () =>{
     generer(objet)
 
 })
+
 btnfilterH.addEventListener("click", async () =>{
     const projet = await fetch("http://localhost:5678/api/works").then(projet=>projet.json());
     const objet = projet.filter(function(x){
@@ -88,34 +66,6 @@ btnfilterT.addEventListener("click", async () =>{
 
 })
 
-
-/*bouton modifier*/
-
-
-function Modifier (){
-    const placeModif = document.querySelector(".titrePortfolio");
-
-    if (Logue != null){
-        const iconBtnModif = document.createElement("i");
-        iconBtnModif.classList.add("fa-solid");
-        iconBtnModif.classList.add("fa-pen-to-square");
-        iconBtnModif.id = "iconeBtnModif";
-        placeModif.appendChild(iconBtnModif);
-
-        const btnModifier = document.createElement("a");
-        btnModifier.innerText = `Modifier`;
-        btnModifier.href = "#modal";
-        btnModifier.id = "btnModifier";
-        placeModif.appendChild(btnModifier);
-    } else {
-        try {
-            placeModif.removeChild(document.getElementById ("iconeBtnModif"));
-            placeModif.removeChild(document.getElementById ("btnModifier"));
-        } catch {}
-        
-    }
-
-}
 
 /*login-logout*/
 
@@ -140,62 +90,5 @@ document.querySelector(".login").addEventListener("click", ()=>{
     document.querySelector(".login").innerHTML = `<a href="formulaire.html">login</a>`;
     document.querySelector(".placemenu").style.visibility = "visible";
     Modifier();
+    homePage();
 })
-
-
-/*création modale*/
-
-
-function CreaModal() {
-    const gju = document.querySelector (".contenuModal");
-
-    const titreModal = document.createElement("h2");
-    titreModal.innerText = `Galerie photos`
-
-    const fermeCroix = document.createElement ("button");
-    fermeCroix.innerText = `X`;
-
-    const photosModal = document.createElement("div");
-
-    async function sde () {
-        const pModal = await fetch("http://localhost:5678/api/works").then(pModal=>pModal.json());
-        genModal (pModal)
-    }
-    
-    function genModal(affiche) {
-        for (let i = 0; i < affiche.length; i++) {
-            const imageElement = document.createElement("img");
-            imageElement.src = affiche[i].imageUrl;
-            imageElement.classList.add ("taillePhotoModal");
-
-            const icontrash = document.createElement("i");
-            icontrash.classList.add("fa-regular");
-            icontrash.classList.add("fa-trash-can");
-            icontrash.id = "iconetrash";
-            imageElement.appendChild(icontrash);
-           
-            photosModal.appendChild(imageElement);
-        }
-    }
-
-    sde()
-
-    const ajoutPhoto = document.createElement("button");
-    ajoutPhoto.innerText = `Ajouter une photo`;
-    ajoutPhoto.classList.add("ajoutPhotoModal");
-
-    gju.appendChild(titreModal);
-    gju.appendChild(fermeCroix);
-    gju.appendChild(photosModal);
-    gju.appendChild(ajoutPhoto);
-
-    fermeCroix.addEventListener("click",() => {
-        const mld = document.querySelector("#modal");
-        mld.style.display = "none";
-        gju.innerHTML ="";
-
-    })
-
-}
-
-
